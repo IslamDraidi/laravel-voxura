15a4ca3bf2a6398907cf7ecf951181c613af3f43
IslamDraidi <islamdraidi85@gmail.com>
2026-03-14 14:09:52 +0200

    updated routes for register: add view and post

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,14 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\UserController;
+use App\Http\Controllers\Auth\RegisterController;
+use App\Http\Controllers\UserController;  // ← add this
 
 Route::get('/', [UserController::class, 'index']);
+
+Route::view('/register', 'auth.register')
+    ->middleware('guest')
+    ->name('register');
+
+Route::post('/register', [RegisterController::class, 'store'])
+    ->middleware('guest');
