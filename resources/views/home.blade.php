f216fd8c2dafdc46c716c260421f39942992d54e
IslamDraidi <islamdraidi85@gmail.com>
2026-03-18 18:35:09 +0200

    Update home view and vite config

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,17 +1,5 @@
 <x-layout mainClass="full-width">
 
-
-    <div class="max-w-2xl mx-auto">
-        <div class="card bg-base-100 shadow mt-8">
-            <div class="card-body">
-                <h1 class="text-3xl font-bold">Welcome to Voxura!</h1>
-                <p class="mt-4 text-base-content/60">This is your brand new voxura application.</p>
-            </div>
-        </div>
-    </div>
-</x-layout>
-
-
     {{-- Hero --}}
     <x-hero />
 
