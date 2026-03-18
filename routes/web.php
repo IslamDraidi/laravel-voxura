401ea1713b40dc9127a7e7ce2dcf1ece0f80f84f
balqeeszeyad <balqeeszeyad562@gmail.com>
2026-03-18 22:01:16 +0300

    update routes

--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,7 @@ use App\Http\Controllers\Auth\RegisterController;
 use App\Http\Controllers\UserController;  
 use App\Http\Controllers\Auth\Logout; 
 use App\Http\Controllers\Auth\Login;
+use App\Http\Controllers\AdminController;
 
 
 Route::get('/', [UserController::class, 'index']);
@@ -29,3 +30,18 @@ Route::view('/login', 'auth.login')
 
 Route::post('/login', [Login::class, 'store'])
     ->middleware('guest');
+
+
+    // Admin Routes
+Route::middleware(['auth', 'admin'])->prefix('admin')->group(function () {
+    Route::get('/', [AdminController::class, 'index']);
+    Route::get('/products/create', [AdminController::class, 'create']);
+    Route::post('/products', [AdminController::class, 'store']);
+    Route::get('/products/{product}/edit', [AdminController::class, 'edit']);
+    Route::put('/products/{product}', [AdminController::class, 'update']);
+    Route::delete('/products/{product}', [AdminController::class, 'destroy']);
+    Route::get('/archive', [AdminController::class, 'archive']);
+Route::post('/products/{id}/restore', [AdminController::class, 'restore']);
+});
+
+
