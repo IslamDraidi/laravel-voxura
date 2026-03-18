a1ee4d6b64b3b45714bdfd4a1dbe0741070d29b5
IslamDraidi <islamdraidi85@gmail.com>
2026-03-18 18:40:42 +0200

    Fix Login.php duplicate code

--- app/Http/Controllers/Auth/Login.php
+++ app/Http/Controllers/Auth/Login.php
@@ -4,17 +4,13 @@ namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
-<<<<<<< Updated upstream
-=======
 use Illuminate\Support\Facades\Auth;
->>>>>>> Stashed changes
 
 class Login extends Controller
 {
     public function store(Request $request)
     {
         $credentials = $request->validate([
-<<<<<<< Updated upstream
             'email'    => 'required|email',
             'password' => 'required',
         ]);
@@ -29,19 +25,3 @@ class Login extends Controller
         ]);
     }
 }
-=======
-            'email'    => ['required', 'email'],
-            'password' => ['required'],
-        ]);
-
-        if (Auth::attempt($credentials)) {
-            $request->session()->regenerate();
-            return redirect()->intended('/');
-        }
-
-        return back()->withErrors([
-            'email' => 'The provided credentials do not match our records.',
-        ])->onlyInput('email');
-    }
-}
->>>>>>> Stashed changes
