116c0a891323295a16ecf672a9783bf3bcaea320
IslamDraidi <islamdraidi85@gmail.com>
2026-03-19 00:01:10 +0200

    Fix web.php routes

--- routes/web.php
+++ routes/web.php
@@ -1,15 +1,15 @@
+<?php
 
-use Illuminate\\Support\\Facades\\Route;
-use App\\Http\\Controllers\\Auth\\RegisterController;
-use App\\Http\\Controllers\\UserController;
-use App\\Http\\Controllers\\Auth\\Logout;
-use App\\Http\\Controllers\\Auth\\Login;
-use App\\Http\\Controllers\\AdminController;
-use App\\Http\\Controllers\\ProductController;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Auth\RegisterController;
+use App\Http\Controllers\UserController;
+use App\Http\Controllers\Auth\Logout;
+use App\Http\Controllers\Auth\Login;
+use App\Http\Controllers\AdminController;
+use App\Http\Controllers\ProductController;
 
 Route::get("/", [UserController::class, "index"]);
 
-// Product Detail
 Route::get("/product/{product}", [ProductController::class, "show"])->name("products.show");
 
 Route::view("/register", "auth.register")
@@ -19,12 +19,10 @@ Route::view("/register", "auth.register")
 Route::post("/register", [RegisterController::class, "store"])
     ->middleware("guest");
 
-// Logout route
 Route::post("/logout", Logout::class)
     ->middleware("auth")
     ->name("logout");
 
-// Login route
 Route::view("/login", "auth.login")
     ->middleware("guest")
     ->name("login");
@@ -32,7 +30,6 @@ Route::view("/login", "auth.login")
 Route::post("/login", [Login::class, "store"])
     ->middleware("guest");
 
-// Admin Routes
 Route::middleware(["auth", "admin"])->prefix("admin")->group(function () {
     Route::get("/", [AdminController::class, "index"])->name("admin.dashboard");
     Route::get("/products/create", [AdminController::class, "create"])->name("admin.products.create");
@@ -42,4 +39,4 @@ Route::middleware(["auth", "admin"])->prefix("admin")->group(function () {
     Route::delete("/products/{product}", [AdminController::class, "destroy"])->name("admin.products.delete");
     Route::get("/archive", [AdminController::class, "archive"])->name("admin.archive");
     Route::post("/products/{id}/restore", [AdminController::class, "restore"])->name("admin.restore");
-});'''
+});
