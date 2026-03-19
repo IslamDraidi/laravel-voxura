66152ed2a5358f6c58de0704b32ef969acb1f101
IslamDraidi <islamdraidi85@gmail.com>
2026-03-19 04:22:44 +0200

    Pass likedIds to home view

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -1,17 +1,22 @@
-<?php 
+<?php
 
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Product;
 
-class UserController extends Controller 
+class UserController extends Controller
 {
     public function index()
     {
-        $products = Product::with('category')->get();
+        $products         = Product::with('category')->get();
         $featuredProducts = $products->take(3);
 
-        return view('home', compact('products', 'featuredProducts'));
+        // Pass liked product IDs so the product card can highlight the heart
+        $likedIds = auth()->check()
+            ? auth()->user()->likedProductIds()
+            : [];
+
+        return view('home', compact('products', 'featuredProducts', 'likedIds'));
     }
 }
