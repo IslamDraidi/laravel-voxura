45e3a2491d5bd25584d2af81fdd77b7a488fdb6d
IslamDraidi <islamdraidi85@gmail.com>
2026-03-21 21:21:02 +0200

    add CartItem.php , Feedback.php , and Orderitem.php

---
+++ app/Models/Cartitem.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class CartItem extends Model
+{
+    protected $fillable = ['cart_id', 'product_id', 'quantity'];
+
+    public function cart()
+    {
+        return $this->belongsTo(ShoppingCart::class, 'cart_id');
+    }
+
+    public function product()
+    {
+        return $this->belongsTo(Product::class);
+    }
+
+    public function subtotal()
+    {
+        return $this->product->price * $this->quantity;
+    }
+}
